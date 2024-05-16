2474cb8d630440d7132446f5bb0136a6c8550b56
Rian de Deus Francisco <riandedeus294@gmail.com>
2024-05-16 15:42:43 -0300

    commit

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+		Scanner Scanner = new Scanner(System.in).useLocale(Locale.US);
+		
+		System.out.println("Por favor, digite o número da Agência !");
+		int numero = Scanner.nextInt();
+		
+		System.out.println("Por favor, digite o nome da Agência !");
+		String agencia = Scanner.next();
+		
+		System.out.println("Por favor, digite o nome do Cliente !");
+		String nomeCliente = Scanner.next();
+		
+		System.out.println("Por favor, digite o saldo da conta !");
+		float saldo = Scanner.nextFloat();
+		
+		
+		System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta "+ numero +" e seu saldo "+ saldo +" já está disponível para saque");
+	
+
+	}
+
+}
