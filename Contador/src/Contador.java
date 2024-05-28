e7aeada25376b85b1d8425ee8072ea273b7ed264
Rian de Deus Francisco <riandedeus294@gmail.com>
2024-05-28 20:06:02 -0300

    commit 1

--- Contador/src/Contador.java
+++ Contador/src/Contador.java
@@ -14,7 +14,7 @@ public class Contador {
 			contar(parametroUm, parametroDois);	
 			
 		} catch (ParametrosInvalidosException exception) {
-            System.out.println("O segundo parâmetro deve ser maior que o primeiro");			
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro ");			
 		}		
     }
 	
